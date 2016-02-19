5410f381a76e78bebf3a7d3299c3cb14953c7009
Thangiee <Thangiee0@gmail.com>
2016-02-19 01:06:41 -0600

    init scalajs script only when on the right page

--- client/src/main/scala/mars/ScalaJSMain.scala
+++ client/src/main/scala/mars/ScalaJSMain.scala
@@ -1,11 +1,16 @@
 package mars
 
 import org.scalajs.jquery.jQuery
+import org.scalajs.dom._
 
 import scala.scalajs.js
 
 object ScalaJSMain extends js.JSApp {
   def main(): Unit = {
-    jQuery(AssistantSearchJS.init _)
+    jQuery(document).find("title").text() match {
+      case title if title contains "Assistants" =>
+        jQuery(AssistantSearchJS.init _)
+      case _ =>
+    }
   }
 }
