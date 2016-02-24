ba98cf33cab105e6ecfdfb0b0db10e82491028a5
Thangiee <Thangiee0@gmail.com>
2016-02-23 23:58:13 -0600

    Call scalajs script directly from html instead of using page title to run said script.

--- client/src/main/scala/mars/ScalaJSMain.scala
+++ client/src/main/scala/mars/ScalaJSMain.scala
@@ -7,10 +7,6 @@ import scala.scalajs.js
 
 object ScalaJSMain extends js.JSApp {
   def main(): Unit = {
-    jQuery(document).find("title").text() match {
-      case title if title contains "Assistants" =>
-        jQuery(AssistantSearchJS.init _)
-      case _ =>
-    }
+
   }
 }
