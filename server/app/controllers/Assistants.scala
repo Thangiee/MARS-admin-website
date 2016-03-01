376581ae31fdd46c8b2ded33ea04bed25f5f8e35
Thangiee <Thangiee0@gmail.com>
2016-03-01 16:30:08 -0600

    Implement edit assistant info dialog. Still need to interface with the backend after submit.

--- server/app/controllers/Assistants.scala
+++ server/app/controllers/Assistants.scala
@@ -3,12 +3,32 @@ package controllers
 import cats.std.all._
 import models.Assistant
 import play.api.Play.current
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.data.format.Formats._
+import play.api.data.validation.Constraints._
 import play.api.i18n.Messages.Implicits._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.json.Json
 import play.api.mvc._
 
+import scala.concurrent.Future
+
 object Assistants extends Controller {
+
+  case class UpdateInfoForm(email: String, empId: String, payRate: Double, job: String, dept: String, title: String, code: String, thres: Double)
+  val updateInfoForm: Form[UpdateInfoForm] = Form(
+    mapping(
+      "email"    -> email,
+      "emp-id"   -> nonEmptyText,
+      "pay-rate" -> of[Double],
+      "job"      -> nonEmptyText,
+      "dept"     -> nonEmptyText,
+      "title"    -> nonEmptyText,
+      "code"     -> nonEmptyText,
+      "thres"    -> of[Double].verifying(min(0.0), max(1.0))
+    )(UpdateInfoForm.apply)(UpdateInfoForm.unapply)
+  )
 
   def page() = Action.async { implicit request =>
     Assistant.all().fold(
@@ -31,12 +51,23 @@ object Assistants extends Controller {
       asst    <- fetchAsst
       faces   <- fetchFaces
     } yield {
-      views.html.assistantDetail(asst, faces.map(_.url))
+      views.html.assistantDetail(asst, faces.map(_.url), updateInfoForm)
     }
 
     res.fold(
       err  => ???, // todo: 404 page
       page => Ok(page)
+    )
+  }
+
+  def update(netId: String) = Action.async { implicit request =>
+    //todo:
+    updateInfoForm.bindFromRequest().fold(
+      err  => Future.successful(???),
+      data => {
+        println(data)
+        Future.successful(Redirect(routes.Assistants.detail(netId)))
+      }
     )
   }
 }
