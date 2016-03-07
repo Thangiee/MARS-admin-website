afe229d2f80a6cb50b3149d1106059869a4eef48
Thangiee <Thangiee0@gmail.com>
2016-03-06 18:13:03 -0600

    Design sign up page

--- server/app/controllers/SignUP.scala
+++ server/app/controllers/SignUP.scala
@@ -2,19 +2,48 @@ package controllers
 
 import javax.inject.Inject
 
-import cats.std.all._
-import models.{Assistant, Instructor}
+import controllers.SignUp.CreateAccForm
+import play.api.data.Form
+import play.api.data.Forms._
 import play.api.i18n.{I18nSupport, MessagesApi}
-import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.mvc._
 
 class SignUp @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
+  val createAccForm = Form(
+    mapping(
+      "netid"      -> nonEmptyText,
+      "username"   -> nonEmptyText,
+      "passwd"     -> nonEmptyText(minLength = 6),
+      "passwd2"    -> nonEmptyText(minLength = 6),
+      "email"      -> email,
+      "last-name"  -> nonEmptyText,
+      "first-name" -> nonEmptyText
+    )(CreateAccForm.apply)(CreateAccForm.unapply)
+  )
+
   def page = Action { implicit request =>
-    Ok(views.html.signUp())
+    Ok(views.html.signUp(createAccForm))
   }
 
   def create = Action { implicit request =>
-    ???
+    val invalidFormMsg = "Invalid form, fix errors and try again."
+
+    createAccForm.bindFromRequest().fold(
+      formErr => BadRequest(views.html.signUp(formErr.withGlobalError(invalidFormMsg))),
+      data    => {
+        if (data.passwd != data.passwd2) {
+          val form = createAccForm.fill(data).withError("passwd", "Does not match").withError("passwd2", "Does not match")
+          BadRequest(views.html.signUp(form.withGlobalError(invalidFormMsg)))
+        } else {
+          // todo:
+          Redirect(routes.Login.page())
+        }
+      }
+    )
   }
+}
+
+object SignUp {
+  case class CreateAccForm(netId: String, username: String, passwd: String, passwd2: String, email: String, lastName: String, firstName: String)
 }
